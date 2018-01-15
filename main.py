f06945e99a7e05250b7ad5c89d604bdae8f53cd2
tobive <knightsofkangouw@gmail.com>
2018-01-15 11:34:31 +0700

    change main.py

--- main.py
+++ main.py
@@ -13,29 +13,29 @@ def main():
     hour = now.hour
     greet_bot.get_updates(new_offset)
     last_update = greet_bot.get_last_update()
-    saved_update_id = last_update['update_id']
+    saved_msg_id = last_update['message']['message_id']
 
     while True:
         greet_bot.get_updates(new_offset)
 
         last_update = greet_bot.get_last_update()
 
-        last_update_id = last_update['update_id']
+        last_msg_id = last_update['message']['message_id']
         last_chat_text = last_update['message']['text']
         last_chat_id = last_update['message']['chat']['id']
         last_chat_name = last_update['message']['chat']['first_name']
 
-        if last_chat_text.lower() in greetings and last_update_id != saved_update_id and 6 <= hour < 12:
+        if last_chat_text.lower() in greetings and last_msg_id != saved_msg_id and 6 <= hour < 12:
             greet_bot.send_message(last_chat_id, 'Good Morning {}'.format(last_chat_name))
-            saved_update_id = last_update_id
+            saved_msg_id = last_msg_id
 
-        elif last_chat_text.lower() in greetings and last_update_id != saved_update_id and 12 <= hour < 17:
+        elif last_chat_text.lower() in greetings and last_msg_id != saved_msg_id and 12 <= hour < 17:
             greet_bot.send_message(last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
-            saved_update_id = last_update_id
+            saved_msg_id = last_msg_id
 
-        elif last_chat_text.lower() in greetings and last_update_id != saved_update_id and 17 <= hour < 23:
+        elif last_chat_text.lower() in greetings and last_msg_id != saved_msg_id and 17 <= hour < 23:
             greet_bot.send_message(last_chat_id, 'Good Evening {}'.format(last_chat_name))
-            saved_update_id = last_update_id
+            saved_msg_id = last_msg_id
         time.sleep(5)
 
 if __name__ == '__main__':
